8c289d44446d64a78e57c27209fbe17342be5368
Anton Julius Rabe <anton197rabe@gmx.de>
2018-06-17 13:17:41 +0200

    Icon/Logo

--- src/main/java/de/sbmltab/view/SBMLTabMainView.java
+++ src/main/java/de/sbmltab/view/SBMLTabMainView.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.image.Image;
 
 
 public class SBMLTabMainView extends Application{
@@ -17,7 +17,7 @@ public class SBMLTabMainView extends Application{
 		Parent root = FXMLLoader.load(getClass().getResource("TabModView.fxml"));
 	    
         Scene scene = new Scene(root, 800, 600);
-    
+        stage.getIcons().add (new Image("File:/Icons/Icon_small.png"));
         stage.setTitle("TabMod v1.0");
         stage.setScene(scene);
         stage.show();
