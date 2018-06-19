594f7f02cca07778ba938dfe382819ffd95f8d9d
Franziska Daumüller <franziska.daumueller@gmail.com>
2018-06-19 21:32:00 +0200

    new tree class added

--- src/main/java/de/sbmltab/view/SBMLTabMainView.java
+++ src/main/java/de/sbmltab/view/SBMLTabMainView.java
@@ -15,28 +15,43 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
 
 public class SBMLTabMainView extends Application {
-	
 
 	public void start(Stage stage) throws Exception {
 
-        MenuBar menuBar = MenuController.generateMenuBar();
-        BorderPane root = new BorderPane();
-        //root.setTop(menuBar);
-		//Parent root = FXMLLoader.load(getClass().getResource("TabModView.fxml"));
-        root.setTop(FXMLLoader.load(getClass().getResource("TabModMenu.fxml")));
-	    root.setCenter(FXMLLoader.load(getClass().getResource("TabModView.fxml")));
-        Scene scene = new Scene(root, 800, 600);
-        
-        stage.getIcons().add (new Image("File:/Icons/Icon_small(16).png"));//add image to the IconList
-	stage.getIcons().add (new Image("File:/Icons/Icon_small(32).png"));//add image to the IconList
-        stage.getIcons().add (new Image("File:/Icons/Icon_small(48).png"));//add image to the IconList
-	stage.getIcons().add (new Image("File:/Icons/Icon_large(256).png"));//add image to the IconList
-        stage.setTitle("TabMod v1.0");
-        stage.setScene(scene);
-        stage.show();
-		
-	
-	}
+		MenuBar menuBar = MenuController.generateMenuBar();
+		BorderPane root = new BorderPane();
+		// root.setTop(menuBar);
+		// Parent root =
+		// FXMLLoader.load(getClass().getResource("TabModView.fxml"));
+		root.setTop(FXMLLoader.load(getClass().getResource("TabModMenu.fxml")));
+		root.setCenter(FXMLLoader.load(getClass().getResource("TabModView.fxml")));
+		root.setLeft(FXMLLoader.load(getClass().getResource("TabModTree.fxml")));
+		Scene scene = new Scene(root, 640, 480);
 
+		stage.getIcons().add(new Image("File:/Icons/Icon_small(16).png"));// add
+																			// image
+																			// to
+																			// the
+																			// IconList
+		stage.getIcons().add(new Image("File:/Icons/Icon_small(32).png"));// add
+																			// image
+																			// to
+																			// the
+																			// IconList
+		stage.getIcons().add(new Image("File:/Icons/Icon_small(48).png"));// add
+																			// image
+																			// to
+																			// the
+																			// IconList
+		stage.getIcons().add(new Image("File:/Icons/Icon_large(256).png"));// add
+																			// image
+																			// to
+																			// the
+																			// IconList
+		stage.setTitle("TabMod 1.0");
+		stage.setScene(scene);
+		stage.show();
+
+	}
 
 }
