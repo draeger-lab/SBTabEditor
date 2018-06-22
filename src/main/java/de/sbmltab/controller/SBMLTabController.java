51327e50b989f9ab85a23f47f3707893835a28fd
Julian Wanner <julian.wanner@student.uni-tuebingen.de>
2018-06-23 01:39:30 +0200

    Implemented Open in JavaFX
    I replaced the open dialog written in Swift by a open Dialoag with JavaFX.

--- src/main/java/de/sbmltab/controller/SBMLTabController.java
+++ src/main/java/de/sbmltab/controller/SBMLTabController.java
@@ -2,79 +2,83 @@ package de.sbmltab.controller;
 
 import java.io.File;
 
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.sbml.jsbml.Reaction;
 import org.sbml.jsbml.SBMLDocument;
 import org.sbml.jsbml.SBMLReader;
-import org.sbml.jsbml.SBMLWriter;
 import org.sbml.jsbml.TidySBMLWriter;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class SBMLTabController {
 
-	private static final transient Logger LOGGER = LogManager.getLogger(SBMLTabController.class);
+  private static final transient Logger LOGGER = LogManager.getLogger(SBMLTabController.class);
 
-	/**
-	 * Save SBML document to a {@link File}.
-	 * 
-	 * @param doc
-	 *            the {@link SBMLDocument} to be written
-	 * @param path
-	 *            absolute path to {@link SBMLDocument}
-	 * @param name
-	 *            name of {@link SBMLDocument}
-	 * @param version
-	 *            version of {@link SBMLDocument}
-	 * 
-	 */
-	public static void save(SBMLDocument doc, File path, String name, String version) {
-		try {
-			TidySBMLWriter.write(doc, path, name, version);
-		} catch (Exception e) {
-			LOGGER.error("Unable to write sbml file", e);
-		}
-	}
+  /**
+   * Save SBML document to a {@link File}.
+   * 
+   * @param doc
+   *            the {@link SBMLDocument} to be written
+   * @param path
+   *            absolute path to {@link SBMLDocument}
+   * @param name
+   *            name of {@link SBMLDocument}
+   * @param version
+   *            version of {@link SBMLDocument}
+   * 
+   */
+  public static void save(SBMLDocument doc, File path, String name, String version) {
+    try {
+      TidySBMLWriter.write(doc, path, name, version);
+    } catch (Exception e) {
+      LOGGER.error("Unable to write sbml file", e);
+    }
+  }
 
-	/**
-	 * Read SBML document from a {@link File}.
-	 * 
-	 * @param path
-	 *            absolute path to {@link SBMLDocument}
-	 */
-	public static String open() {
-		String filePath = "";
-		FileNameExtensionFilter xmlFilter = new FileNameExtensionFilter("XML File","xml");
-		FileNameExtensionFilter sbmlFilter = new FileNameExtensionFilter("SBML File","SBML");
-		final JFileChooser fileChooser = new JFileChooser();
-		fileChooser.addChoosableFileFilter(xmlFilter);
-		fileChooser.addChoosableFileFilter(sbmlFilter);
-        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-        int result = fileChooser.showOpenDialog(null);
-        if (result == JFileChooser.APPROVE_OPTION) {
-        	filePath = fileChooser.getSelectedFile().getPath();
-        }
-        return filePath;
-	}	
-	public static SBMLDocument read(File path) {
-		SBMLDocument doc = null;
-		try {
-			doc = SBMLReader.read(path);
-		} catch (Exception e) {
-			LOGGER.error("Unable to read sbml file", e);
-		}
-		return doc;
-	}
-	public static boolean validate(SBMLDocument doc) {
-		if(doc.checkConsistencyOffline() == 0) {
-			return true;
-		}else {
-			for(int i = 0; i < doc.checkConsistencyOffline(); i++) {
-				LOGGER.error(doc.getError(i));
-			}
-			return false;
-		}
-	}
+  /*
+   * @author Julian Wanner
+   * Returns absolute Path of a chosen file with JavaFX.
+   */
+  public static String open(Stage stage) {
+    final FileChooser fileChooser = new FileChooser();
+    String filePath = "";
+    fileChooser.getExtensionFilters().addAll(
+      new ExtensionFilter("XML Files", "*.xml"),
+      new ExtensionFilter("SBML Files", "*.SBML"));
+    fileChooser.setTitle("Choose SBML or XML File.");
+    fileChooser.setInitialDirectory(new File(System.getProperty("user.home"))) ;
+    File file = fileChooser.showOpenDialog(stage);
+    if (file != null) {
+      filePath = file.getAbsolutePath();
+    }
+    return filePath;
+  }
+
+  /**
+   * Read SBML document from a {@link File}.
+   * 
+   * @param path
+   *            absolute path to {@link SBMLDocument}
+   */
+  public static SBMLDocument read(String filePath) {
+    SBMLDocument doc = null;
+    try {
+      doc = SBMLReader.read(filePath);
+    } catch (Exception e) {
+      LOGGER.error("Unable to read sbml file", e);
+    }
+    return doc;
+  }
+  public static boolean validate(SBMLDocument doc) {
+    if(doc.checkConsistencyOffline() == 0) {
+      return true;
+    }else {
+      for(int i = 0; i < doc.checkConsistencyOffline(); i++) {
+        LOGGER.error(doc.getError(i));
+      }
+      return false;
+    }
+  }
 }
