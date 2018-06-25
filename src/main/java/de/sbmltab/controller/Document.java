56ff65b32b01e34f3810cc29d2e940a24dac6a05
Melina Maier <melina.j.maier@web.de>
2018-06-25 19:54:05 +0200

    Changes in Document class
    implements interface TreeNodeChangeListener

--- src/main/java/de/sbmltab/controller/Document.java
+++ src/main/java/de/sbmltab/controller/Document.java
@@ -1,43 +1,82 @@
 package de.sbmltab.controller;
 
+import java.beans.PropertyChangeEvent;
 import java.io.File;
+import javax.swing.tree.TreeNode;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.AbstractTreeNode;
+import org.sbml.jsbml.SBMLReader;
+import org.sbml.jsbml.TidySBMLWriter;
+import org.sbml.jsbml.util.TreeNodeChangeListener;
+import org.sbml.jsbml.util.TreeNodeRemovedEvent;
 
-public class Document<T> {
-	
+public class Document<T> implements TreeNodeChangeListener {
+
 	private static final transient Logger LOGGER = LogManager.getLogger(SBMLTabController.class);
-	
+
 	private T tempDoc;
-	
-	private File pathtempDoc;
-	
-	boolean changed = false; 
-	
+
+	private String pathtempDoc;
+
+	boolean changed;
+
 	/**
 	 * Constructor for Document
 	 *
-	 * @param tempDoc
+	 * @param Doc
 	 * 
-	 * @param pathtempDoc
+	 * @param version
 	 */
-	public Document(T tempDoc, File pathtempDoc){
-		this.tempDoc = tempDoc;
-		this.pathtempDoc = pathtempDoc;
+	public Document(T Doc, String version) {
+		try {
+			File temp = File.createTempFile("temp-file-name", ".tmp");			
+			String absolutePath = temp.getAbsolutePath();
+			pathtempDoc = absolutePath.substring(0, absolutePath.lastIndexOf(File.separator));
+			
+//			TidySBMLWriter.write(Doc, pathtempDoc, "temp-file-name", version);			
+//			tempDoc = SBMLReader.read(new File(tempfilePath));	
+
+//			((AbstractTreeNode) tempDoc).addTreeNodeChangeListener(this);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		changed = false;
 	}
-	
+
 	/**
-	 * Edit a temporary document 
+	 * Edit a temporary document
 	 * 
 	 * @param x
 	 */
 	public void edit(String x) {
 		try {
 			changed = true;
-			
+
 		} catch (Exception e) {
 			LOGGER.error("Unable to edit sbml file", e);
 		}
-	}	
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		System.out.println("change");
+		changed = true;
+	}
+
+	@Override
+	public void nodeAdded(TreeNode node) {
+		System.out.println("new entry");
+		changed = true;
+	}
+
+	@Override
+	public void nodeRemoved(TreeNodeRemovedEvent event) {
+		TreeNode parent = event.getPreviousParent();
+		System.out.println("Child of " + parent.toString() + " removed");
+		changed = true;
+	}
+
 }
