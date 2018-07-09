03da0d114ce28132b18a13a033ce18300ff36219
MelinaMaier <melina.j.maier@web.de>
2018-07-09 13:22:52 +0200

    add button to expand/collapse tree
    closes #33

--- src/main/java/de/sbtab/view/SBTabTreeController.java
+++ src/main/java/de/sbtab/view/SBTabTreeController.java
@@ -8,8 +8,10 @@ import javax.swing.tree.TreeNode;
 import org.sbml.jsbml.SBase;
 
 import de.sbtab.controller.SBTabController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 
@@ -17,13 +19,17 @@ public class SBTabTreeController implements Initializable {
 
 	@FXML
 	private TreeView<String> treeView;
-
 	
-//	public String getResource(ResourceBundle resources, String value) {
-//		resources = ResourceBundle.getBundle("");
-//		return resources.getString(value);
-//		}
-		
+	@FXML
+	private Button expandButton;
+	
+	public Boolean expanded = false;
+	
+	@FXML
+	void doExpandTree(ActionEvent event) {
+		expanded ^= true;
+		ExpandTree2();
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle value) {
@@ -32,31 +38,34 @@ public class SBTabTreeController implements Initializable {
 		TreeItem<String> root2 = new TreeItem<String>(String.valueOf(root));
 		treeView.setRoot(root2);
 
-		tree(root, document, root2);
+		tree(root, document, root2, false);
 	}
 
-	private void tree(TreeNode root, SBase document, TreeItem<String> root2) {
+	public void tree(TreeNode root, SBase document, TreeItem<String> root2, Boolean expand) {
 		try {
 			if (root.getChildCount() > 0) {
 				for (int i = 0; i < root.getChildCount(); i++) {
 					TreeNode node = root.getChildAt(i);
 					TreeItem<String> node2 = new TreeItem<String>(String.valueOf(node));
+					root2.setExpanded(expand);
 					root2.getChildren().add(node2);
-					tree(node, document, node2);
+					tree(node, document, node2, expand);
 				}
 			}
-
-			// for (int i = 0; i < document.getChildCount(); i++) {
-			// Enumeration<TreeNode> children = document.getChildAt(i).children();
-			// while (children.hasMoreElements()) {
-			// TreeItem<String> node = new TreeItem<String>(String.valueOf(children.nextElement()));
-			// root.getChildren().add(node);
-			// }
-			// }
 
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 
+	}
+	
+	public void ExpandTree2(){
+		SBase document = SBTabController.getDoc();
+		TreeNode root = document.getRoot();
+		TreeItem<String> root2 = new TreeItem<String>(String.valueOf(root));
+		root2.setExpanded(expanded);
+		treeView.setRoot(root2);
+
+		tree(root, document, root2, expanded);
 	}
 }
