db05dc1ef2d8101f86d4b8c7a4a13d1a41dd3da8
Mykola Zakharchuk <zakharchuk.vn@gmail.com>
2018-07-14 19:06:06 +0200

    Save generated tables in Producer

--- src/main/java/de/sbtab/services/SBTabTableProducer.java
+++ src/main/java/de/sbtab/services/SBTabTableProducer.java
@@ -1,18 +1,22 @@
 package de.sbtab.services;
 
+import java.util.HashMap;
+
 import org.sbml.jsbml.SBase;
 
+import de.sbtab.containers.SBTabCompartmentWrapper;
+import de.sbtab.containers.SBTabReactionWrapper;
+import de.sbtab.containers.SBTabSpeciesWrapper;
 import javafx.scene.control.TableView;
 
 /**
  * Created to generate the desired TableView. 
  * In the future should be connected with TreeView navigation.
- * TODO: corresponding TableView must be saved in order to not generate TableView 
- * 		 repeatedly after each user interaction with TreeView
  * 
  * */
 public class SBTabTableProducer {
 	private SBase doc;
+	private HashMap<TableType, TableView<?>> tablePull = new HashMap<>();
 
 	public SBTabTableProducer(SBase doc) {
 		super();
@@ -24,17 +28,24 @@ public class SBTabTableProducer {
 	 * @return TableView or null
 	 * */
 	public TableView<?> getTableView(TableType type) {
-		switch (type) {
-		case REACTION:
-			return new SBTabReactionTable(doc).makeTableView();
-		case COMPARTEMENT:
-			return new SBTabCompartmentTable(doc).makeTableView();
-		case SPECIE:
-			return new SBTabSpeciesTable(doc).makeTableView();
-		// TODO: add cases for another table factories
-		default:
-			break;
+		if (tablePull.containsKey(type)) {
+			return tablePull.get(type);
+		} else {
+			switch (type) {
+			case REACTION:
+				tablePull.put(TableType.REACTION, new SBTabReactionTable(doc).makeTableView());
+				return tablePull.get(type);
+			case COMPARTEMENT:
+				tablePull.put(TableType.COMPARTEMENT, new SBTabCompartmentTable(doc).makeTableView());
+				return tablePull.get(type);
+			case SPECIE:
+				tablePull.put(TableType.SPECIE, new SBTabSpeciesTable(doc).makeTableView());
+				return tablePull.get(type);
+			// TODO: add cases for another table factories
+			default:
+				break;
+			}
+			return null;
 		}
-		return null;
 	}
 }
