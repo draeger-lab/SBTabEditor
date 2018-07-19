307eeba5698ada046a246bfeae53d6d567d20fae
Mykola Zakharchuk <zakharchuk.vn@gmail.com>
2018-07-19 20:46:42 +0200

    Rearrange tests into suites

---
+++ src/test/java/de/sbtab/integration/SBTabIntegrationTests.java
@@ -0,0 +1,14 @@
+package de.sbtab.integration;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	SBTabLaunchTest.class
+})
+public class SBTabIntegrationTests {
+	// the class remains empty,
+	// used only as a holder for the above annotations
+}
